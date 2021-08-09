160dcac513a0a1a8ff4ffa2377227ba796fedba8
Silvia <sleyvarangel@gmail.com>
2021-08-09 09:09:56 -0700

    Trabajo terminado

---
+++ js/script.js
@@ -0,0 +1,5 @@
+
+alert ("¡Bienvenvidas mujeres emprendedoras!");
+if (confirm("¿Deseas saber acerca de Autonomía Económica de las Mujeres?"));
+var nombre = prompt ("Introduce tu nombre por favor:");
+alert ("Hola "+nombre+" "+ "te invitamos a ser emprendedora y a desarrollar tu autonomía económica");
